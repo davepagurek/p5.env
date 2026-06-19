9d2a10dd8cb2ab6a448cacc24a630eae45ab1569
Dave Pagurek <davepagurek@gmail.com>
2026-06-19 08:28:57 -0700

    Update example

--- sketch.js
+++ sketch.js
@@ -1,24 +1,25 @@
 p5.disableFriendlyErrors = true
 
 let myShader
+let pano
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL)
   pixelDensity(1)
 
-  myShader = buildEnvLightShader(() => {
+  const env = () => {
     envColor.begin()
-    let ground = uniformVec3(color('#996d5b'))
+    let ground = uniformVec3(color('#564943'))
 
     // Sun orbits in the x-y plane. At angle 0 the sun is at the zenith [0,-1,0].
-    let sunAngle = millis() * 0.0003
+    let sunAngle =  PI * 0.45 // millis() * 0.0003
     let sunDir = normalize(vec3(sin(sunAngle), cos(sunAngle) * -1, 0))
     // 1 at noon, fades to 0 at the horizon, stays 0 through the night
     let daylight = map(dot(sunDir, [0, -1, 0]), -1, 1, 0, 1)
     ground *= map(daylight, 0.2, 0.6, 0, 1, true)
     let nightSkyColor = vec3(0.08, 0.08, 0.25)
 
-    let sunColor     = mix(nightSkyColor, vec3(4.0, 3.5, 2.0) * 0.7,  daylight)
+    let sunColor     = mix(nightSkyColor, vec3(4.0, 3.5, 2.0) * 2,  daylight)
     let skyColor     = mix(nightSkyColor, vec3(0.5, 0.75, 1.0), daylight)
     let horizonColor = mix(mix(nightSkyColor, vec3(0.5, 0.2, 0.45), map(daylight, 0.2, 0.5, 0, 1, true)), vec3(0.3, 0.55, 1.0) * 0.7 + 0.4, daylight)
 
@@ -30,7 +31,7 @@ function setup() {
 
     // Sun is the base layer, sky goes on top, ground on top of both
     let l = envLight(sky, envColor.dir, envColor.blur)
-    l.mix(l.envCircle(sunDir, PI * 0.04), sunColor)
+    l.mix(l.envCircle(sunDir, PI * 0.02), sunColor)
     l.mix(l.envCircle([0, 1, 0], PI * 0.5), ground)
 
     // l.mix(l.envWindow(normalize([1, -0.5, 0]), [PI*0.2, PI*0.35], [2, 2], PI*0.05), vec3(1.4))
@@ -46,13 +47,16 @@ function setup() {
 
     envColor.set(l.get())
     envColor.end()
-  })
+  }
+  myShader = buildEnvLightShader(env)
+  pano = buildEnvLightPanorama(env)
   console.log(myShader.fragSrc())
 }
 
 function draw() {
   background(255)
   orbitControl()
+  panoramaEnv(pano)
   scale(min(width / 600, height / 600))
   noStroke()
   shader(myShader)
@@ -64,8 +68,9 @@ function draw() {
         map(i, 0, n, -400*0.35, 400*0.35),
         map(j, 0, n, -400*0.35, 400*0.35),
       )
-      fill(0.8 * map(j, 0, n, 50, 205))
-      specularMaterial(map(j, 0, n, 205, 50))
+      fill(200, 20, 20)
+      specularMaterial(50)
+      metalness(pow(map(j, 0, n, 0, 1), 2) * 200)
       shininess(pow(map(i, 0, n, 0, 1), 2) * 400)
       // shininess(map(mouseX, 0, width, 0, 200))
       sphere(40)
